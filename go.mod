635adeebffa3bc2b1ec4144a4423ed9a827f0abc
Mike Webster <mwebster@wyzant.com>
2020-04-08 18:27:07 -0500

    use gin to setup server

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/mike-webster/repo-watcher
 
 require (
-	github.com/gin-gonic/gin v1.6.2 // indirect
+	github.com/gin-gonic/gin v1.6.2
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
 	repos.wyzdev.com/wyzant-web/citizen v0.5.0
 )
--- server.go
+++ server.go
@@ -1,11 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
 
-type server struct{}
+	"github.com/gin-gonic/gin"
+)
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"hello world"}`))
+const CODE_OK int = 200
+const CODE_INVALID int = 400
+const CODE_UNAUTH int = 401
+
+func startServer(port int) {
+	r := gin.Default()
+	r.GET("/", handlerHealtcheck)
+
+	r.Group("/v1")
+	{
+		r.POST("/github", handlerGitHub)
+	}
+	r.Run(fmt.Sprintf(":%v", port))
+}
+
+func handlerHealtcheck(ctx *gin.Context) {
+	ctx.JSON(CODE_OK, fmt.Sprintf("{\"%v\":\"%v\"}", "message", "ok"))
+}
+
+func handlerGitHub(ctx *gin.Context) {
+	eventName := ctx.Request.Header["X-GitHub-Event"]
+	secret := ctx.Request.Header["X-Hub-Signature"]
+	body := ctx.Request.PostForm
+	message := "Incoming GH WH request:\n\tevent: %v,\n\tsecret: %v, \n\tbody: %v"
+	Log(fmt.Sprintf(message, eventName, secret, body), "info")
 }

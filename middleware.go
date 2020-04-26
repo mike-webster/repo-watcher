40712e43f9a1f029a166bc5c8f16673e5fb51a9d
Mike Webster <mwebster@wyzant.com>
2020-04-26 14:34:35 -0500

    move middleware

--- middleware.go
+++ middleware.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"runtime/debug"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -35,5 +38,54 @@ func setDependencies(deps *AppDependencies) gin.HandlerFunc {
 		ctx.Set("deps", deps)
 		ctx.Set("logger", defaultLogger(ctx))
 		ctx.Next()
+	}
+}
+
+func requestLogger() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		defer func(ctx *gin.Context) logrus.FieldLogger {
+			if ctx.Request.URL.Path == "/" && ctx.Writer.Status() == 200 {
+				return nil
+			}
+
+			// log body if one is given]
+			strBody := ""
+			body, err := ioutil.ReadAll(ctx.Request.Body)
+			if err != nil {
+				defaultLogger(ctx).WithField("error", err).Error("cant read request body")
+			} else {
+				// write the body back into the request
+				ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+				strBody = string(body)
+				strBody = strings.Replace(strBody, "\n", "", -1)
+				strBody = strings.Replace(strBody, "\t", "", -1)
+			}
+
+			logger := defaultLogger(ctx).WithFields(logrus.Fields{
+				"client_ip":    ctx.ClientIP(),
+				"event":        "http.in",
+				"method":       ctx.Request.Method,
+				"path":         ctx.GetString("originalPath"),
+				"query":        ctx.Request.URL.RawQuery,
+				"referer":      ctx.Request.Referer(),
+				"status":       ctx.Writer.Status(),
+				"user_agent":   ctx.Request.UserAgent(),
+				"git_event":    ctx.Request.Header.Get("X-GitHub-Event"),
+				"request_body": strBody,
+			})
+
+			if len(ctx.Errors) > 0 {
+				logger.Error(strings.TrimSpace(ctx.Errors.String()))
+			} else {
+				logger.Info()
+			}
+
+			ctx.Set("logger", defaultLogger(ctx).WithFields(logrus.Fields{
+				"git_event": ctx.Request.Header.Get("X-GitHub-Event"),
+			}))
+
+			return logger
+		}(ctx)
 	}
 }

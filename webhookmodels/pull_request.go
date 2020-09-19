71442f4abee2305b7787f10e3fa5e8d7a963aa9e
Mike Webster <mwebster@wyzant.com>
2020-09-19 10:05:24 -0500

    add properties

--- webhookmodels/pull_request.go
+++ webhookmodels/pull_request.go
@@ -4,14 +4,17 @@ import "time"
 
 // PullRequest represents a github pull request
 type PullRequest struct {
-	ID        int64      `json:"id"`
-	NodeID    string     `json:"node_id"`
-	URL       string     `json:"html_url"`
-	Number    int        `json:"number"`
-	State     string     `json:"state"`
-	Title     string     `json:"title"`
-	User      User       `json:"user"`
-	Body      string     `json:"body"`
+	ID     int64  `json:"id"`
+	NodeID string `json:"node_id"`
+	URL    string `json:"html_url"`
+	Number int    `json:"number"`
+	State  string `json:"state"`
+	Title  string `json:"title"`
+	User   User   `json:"user"`
+	Body   string `json:"body"`
+	Head   struct {
+		Branch string `json:"ref"`
+	} `json:"head"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 	Assignee  User       `json:"assignee"`

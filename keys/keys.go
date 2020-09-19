7f38f91fe1d39faf9ef480c2f4acfc7540c45eff
Mike Webster <mwebster@wyzant.com>
2020-09-19 10:33:20 -0500

    add method to retrieve values from context

--- keys/keys.go
+++ keys/keys.go
@@ -1,5 +1,7 @@
 package keys
 
+import "context"
+
 type ContextKey string
 
 var (
@@ -7,3 +9,20 @@ var (
 	TeamCitySlackResponseURL ContextKey = "slack-url"
 	AutoMerge                ContextKey = "auto-merge"
 )
+
+// Get will attempt  to retrieve the value from  the  context, first by using the
+// key value. If that yields no results the  return value is the result of attempting
+// to  retrieve the string value of the key.
+// Note: This  is helpful for dealing  with gin contexts.
+func Get(ctx context.Context, key ContextKey) interface{} {
+	if ctx == nil {
+		return nil
+	}
+
+	iVal := ctx.Value(key)
+	if iVal != nil {
+		return iVal
+	}
+
+	return ctx.Value(string(key))
+}
